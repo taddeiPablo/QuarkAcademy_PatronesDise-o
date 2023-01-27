20bf75eb6747053615f202aaa1c6e356f2c08fc2
pablo <taddeiApablo23@gmail.com>
2023-01-27 12:05:34 -0300

    agregando otro patron Memento

---
+++ QuarkAcademy_Patrones_Implementaciones/AppMemento/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MementoCoreLibrary;
+
+namespace AppMemento
+{
+    class Program
+    {
+        static CareTaker caretaker = new CareTaker();
+        static void Main(string[] args)
+        {
+            var p = new Persona();
+            p.Nombre = "Pepe";
+
+
+            caretaker.add(p.saveToMemento());
+
+            p.Nombre = "Pepe1";
+            caretaker.add(p.saveToMemento());
+
+
+            p.Nombre = "Pepe2";
+            caretaker.add(p.saveToMemento());
+
+
+            Memento m1 = caretaker.GetMemento(0);
+            Console.WriteLine("Viendo memento 1: " + m1.Estado);
+            Console.WriteLine("Viendo memento 2: " + caretaker.GetMemento(1).Estado);
+            Console.WriteLine("Viendo memento 3: " + caretaker.GetMemento(2).Estado);
+
+
+            p.restoreToMemento(m1);
+
+            p.restoreToMemento(caretaker.GetMemento(1));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuarkAcademy_Patrones_Implementaciones/MementoCoreLibrary/CareTaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoCoreLibrary
+{
+    public class CareTaker
+    {
+        private List<Memento> _mementos = new List<Memento>();
+
+        public void add(Memento memento)
+        {
+            _mementos.Add(memento);
+        }
+        public Memento GetMemento(int index)
+        {
+            return _mementos[index];
+        }
+    }
+}
